d50a2bd8a41edbe576962e8674589ff046325e20
NBTaco <menandor119@gmail.com>
2024-10-15 12:00:34 +0200

    gombok dinamikus hozzáadása

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
 const display = document.getElementById("display")
+const keysDiv = document.getElementById("keys")
+
+
+const buttons = [
+    {label: '+', class: 'operator-btn'},
+    {label: '1'},
+    {label: '2'},
+    {label: '3'},
+    {label: '-', class: 'operator-btn'},
+    {label: '4'},
+    {label: '5'},
+    {label: '6'},
+    {label: '*', class: 'operator-btn'},
+    {label: '7'},
+    {label: '8'},
+    {label: '9'},
+    {label: '/', class: 'operator-btn'},
+    {label: '0'},
+    {label: '.'},
+    {label: '^2', class: 'operator-btn'},
+    {label: 'C', class: 'a', action: 'clear'},
+    {label: '=', class: 'a', action: 'calculate'}
+]
+
+
+buttons.forEach(button => {
+    const btn = document.createElement("button")
+    btn.textContent = button.label;
+    keys.appendChild(btn);
+});
+
+
 
 // Szamok / irasjelek hozzáadása a kijelőhöz
 function appendToDisplay(input){
@@ -12,8 +44,6 @@ function clearDisplay(){
 
 // Szamolas
 function calculate(){
-
-
 
     try{
         display.value = eval(display.value)
